b7e4e9a26a73e9f0d0a714632a93fbf3261f51ed
yuwongyu <rkwkblues@gmail.com>
2016-07-05 15:35:30 +0900

    회원가입 작업중 - 일반회원, 사업자 컴포넌트 생성
    Signed-off-by: yuwongyu <rkwkblues@gmail.com>

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
-    selector: 'my-app',
+    selector: 'app',
     template: '<h1>My First Angular 2 App</h1>'
 })
 export class AppCmp { }
--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,13 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { enableProdMode, provide } from '@angular/core';
+import { ROUTER_PROVIDERS } from '@angular/router';
+
 import { AppCmp } from './app';
-bootstrap(AppCmp);
+
+if ('<%= ENV %>' === 'prod') { enableProdMode(); }
+
+bootstrap(AppCmp, [
+    ROUTER_PROVIDERS,
+    provide(APP_BASE_HREF, { useValue: '<%= APP_BASE %>' })
+]);
